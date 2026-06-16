6811c5e6c03bec67fb9b8721149eb3703f4c5709
Dean Harley Pal <paldeanharley@gmail.com>
2026-06-16 09:06:46 +0800

    refactor: swap tailwind grid classes for movie grid css class - grid logic moved to index.css, Renders 5 columns on desktop, autofill between, and 2 column for mobile layout

--- p3-react-app/src/components/MovieGrid.jsx
+++ p3-react-app/src/components/MovieGrid.jsx
@@ -2,11 +2,10 @@ import MovieCard from './MovieCard';
 
 export default function MovieGrid({movies}) {
   return (
-    <div className='grid grid-cols-2 lg:grid-cols-5 gap-5'>
+    <div className='movie-grid'>
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
- 
     </div>
   )
 }
