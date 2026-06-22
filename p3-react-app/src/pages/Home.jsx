d385102599811cec2e0b5c839d7ade4f537487f8
Dean Harley Pal <paldeanharley@gmail.com>
2026-06-22 20:06:50 +0800

    refactor: drop loading delay, simplify into two sections - remove setTimeout based loading state. Now renders NowPlaying and Popular directly

--- p3-react-app/src/pages/Home.jsx
+++ p3-react-app/src/pages/Home.jsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from 'react'
 import ScrollTopButton from '../components/ScrollTopButton'
 import NowPlaying from './NowPlaying'
 import Popular from './Popular'
-import { PageLoader } from '../components/Spinner';
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
 
   const currentDate = new Date().toLocaleDateString(undefined, {
     weekday: 'long',
@@ -13,36 +10,17 @@ export default function Home() {
     day: 'numeric',
   })
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 200)
-
-    return () => clearTimeout(timer)
-  }, [])
-
   return (
     
-    <main className='py-10 min-h-screen'>
-      <header>
-        <p className='text-lg text-center text-gray-400 italic'>
-          Today is {currentDate}</p>
-      </header>
+    <main className='home-page'>
+      <div className='home-date'>{currentDate}</div>
+      <section className='home-section'>
+        <NowPlaying />
+      </section>
 
-      {loading ? (
-          <PageLoader />
-        ) : (
-          <>
-            <section>
-              <NowPlaying />
-            </section>
-
-            <section>
-              <Popular />
-            </section>
-          </>
-        )}
-
+      <section className='home-section'>
+        <Popular />
+      </section>
       <ScrollTopButton />
     </main>
     
